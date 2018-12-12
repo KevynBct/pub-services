0b00a0dc6cd56ea46a14c772e83620924a84179a
Kevyn <kevyn.buchet@gmail.com>
2018-12-12 09:54:24 +0100

    Fix Bug

--- pub-rest-api/index.js
+++ pub-rest-api/index.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
-const pubService = require('pub-services').pubService;
+const pubsServices = require('pub-services').pubsServices;
 const app = express();
 
 app.get('/pubs', function (req, res) {
-  const pubs = pubService.getAllPubs();
-  res.status(200).json();
+  const pubs = pubsServices.getAllPubs();
+  res.status(200).json(pubs);
+
 })
 
 var server = app.listen(3000, function() {
